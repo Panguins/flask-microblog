92551b8499b996dba35ed634e0abf48513567184
sub7x22 <panguins1610@hotmail.com>
2021-11-04 05:29:40 -0400

    added mail server, error handling, followers

--- config.py
+++ config.py
@@ -6,3 +6,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('587') or 25)
+    MAIL_USE_TLS = os.environ.get('1') is not None
+    MAIL_USERNAME = os.environ.get('panguins@gmail.com')
+    MAIL_PASSWORD = os.environ.get('test')
+    ADMINS = ['sub7x@protonmail.com']
